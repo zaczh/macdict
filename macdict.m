8531dba35f657e16917fb2f77315dc8ec31e9a61
Jak Wings <jakwings@gmail.com>
2014-04-11 10:15:02 +0800

    fix macdict.m

--- macdict.m
+++ macdict.m
@@ -7,10 +7,16 @@ int main(int argc, const char *argv[])
             printf("Usage: %s <words>\n", argv[0]);
             return -1;
         }
-        NSString *search = [NSString stringWithCString: argv[1] encoding: NSUTF8StringEncoding];
-        CFStringRef def =  DCSCopyTextDefinition(NULL,(CFStringRef)search,CFRangeMake(0, [search length]));
-        NSString *output = [NSString stringWithFormat: @"Definitions of <%@>\n%@\n", search, (NSString *)def];
-        printf("%s", [output UTF8String]);
+        NSMutableArray *words = [NSMutableArray arrayWithCapacity: (argc - 1)];
+        for (int i = 1; i < argc; i++) {
+          [words addObject: [NSString stringWithCString: argv[i] encoding: NSUTF8StringEncoding]];
+        }
+        DCSDictionaryRef s_dict = NULL;
+        CFStringRef s = (CFStringRef)[words componentsJoinedByString: @" "];
+        CFRange s_range = DCSGetTermRangeInString(s_dict, s, 0);
+        CFStringRef s_definition = DCSCopyTextDefinition(s_dict, s, s_range);
+        NSString *definition = [NSString stringWithFormat: @"Definitions of <%@>\n%@", s, (NSString *)s_definition];
+        printf("%s\n", [definition UTF8String]);
     }
     return 0;
 }
