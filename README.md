a088faf1388aedf6e48b5a08728b574f8ab85f99
Jak Wings <jakwings@gmail.com>
2014-04-15 17:19:08 +0800

    macdict2 is release

--- README.md
+++ README.md
@@ -1,11 +1,102 @@
+macdict2
+========
+
+The second version of macdict.
+
+Installation
+------------
+
+for Objective-C executable file:
+
+> `make m`
+
+Usage
+-----
+
+`macdict2 [[-d dictionary], ...] <words>`
+
+### Options: `-h`
+
+Show help information and exit.
+
+### Options: `-l`
+
+Show a list of names of all available dictionaries.
+
+### Options: `-d <dictionary>`
+
+`dictionary` is the name of the dictionary.
+
+If `dictionary` is `all`, then macdict2 will search all available dictionaries and show all the definitions of the word or phrase.
+
+If no dictionary is specified, macdict2 will search all available dictionaries and show the first definition found.
+
+e.g.
+
+```
+$ macdict2 Quod Erat Demonstrandum
+Definitions of <Quod Erat Demonstrandum>
+quod erat demonstrandum |kwɒd ˌɛrat dɛmənˈstrandʊm | (abbrev.: QED) ▶exclamation used to convey that a fact or situation demonstrates the truth of one's theory or claim, especially to mark the conclusion of a formal proof. ORIGIN Latin, literally ‘which was to be demonstrated’.
+```
+
+```
+$ macdict -d all Quod Erat Demonstrandum
+Definitions of <Quod> in {Prisma woordenboek Nederlands}
+quod |kwot| < Latijnvoornaamwoordwat, hetgeen~ erat demonstrandumhetgeen bewezen moest worden~ nonwat niet het geval is: als ik een nieuwe baan zou willen - ~ non - dan zou het iets in het onderwijs zijn~ licetwat toegestaan, geoorloofd is
+
+
+Definitions of <Quod Erat Demonstrandum> in {スーパー大辞林}
+Q.E.D.〖ラテン quod erat demonstrandum 〗「（このように）証明されるべきであったところの」の意。また，数学や物理で，「よって証明された」の意。
+
+
+Definitions of <Quod Erat Demonstrandum> in {Oxford Dictionary of English}
+quod erat demonstrandum |kwɒd ˌɛrat dɛmənˈstrandʊm | (abbrev.: QED) ▶exclamation used to convey that a fact or situation demonstrates the truth of one's theory or claim, especially to mark the conclusion of a formal proof. ORIGIN Latin, literally ‘which was to be demonstrated’.
+
+
+Definitions of <Quod Erat Demonstrandum> in {New Oxford American Dictionary}
+quod erat demonstrandum |kwäd ˈerət ˌdemənˈsträndəm | (abbr.: QED) ▶noun used to convey that a fact or situation demonstrates the truth of one's theory or claim, esp. to mark the conclusion of a formal proof. ORIGIN Latin, literally ‘which was to be demonstrated.’
+
+
+Definitions of <Quod Erat Demonstrandum> in {New Ace English-Korean Dictionary and New Ace Korean-English Dictionary}
+quod erat demonstrandum |kwɔ̀ːd érɑːt dèmənstrǽndəm, dèimɔːnstrɑ́ːndum|｛라틴어｝그 일은 증명되었어야 마땅했다.약어 Q.E.D.
+```
+
+```
+macdict -d 'New Oxford American Dictionary' Quod Erat Demonstrandum
+Definitions of <Quod Erat Demonstrandum> in {New Oxford American Dictionary}
+quod erat demonstrandum |kwäd ˈerət ˌdemənˈsträndəm | (abbr.: QED) ▶noun used to convey that a fact or situation demonstrates the truth of one's theory or claim, esp. to mark the conclusion of a formal proof. ORIGIN Latin, literally ‘which was to be demonstrated.’
+```
+
+```
+macdict -l                                                         
+Prisma woordenboek Nederlands
+スーパー大辞林
+Oxford Dictionary of English
+New Ace Korean Language Dictionary
+Oxford American Writer's Thesaurus
+New Oxford American Dictionary
+Multidictionnaire de la langue française
+New Ace English-Korean Dictionary and New Ace Korean-English Dictionary
+Diccionario General de la Lengua Española Vox
+ウィズダム英和辞典 / ウィズダム和英辞典
+維基百科
+Oxford Thesaurus of English
+Oxford Chinese Dictionary
+Dizionario italiano da un affiliato di Oxford University Press
+Apple 辭典
+```
+
+- - -
+
 macdict
 =======
 
 A commandline tool for Mac OS X to look up words in the builtin/user dictionaries.
 
+Supports: OS X v10.5 and later
 
-Install
-=======
+Installation
+------------
 
 for C executable file:
 
@@ -23,9 +114,8 @@ for Shell executable file:
 
 > `make sh`
 
-
 Usage
-=====
+-----
 
 `macdict <words>`
 
---
+++ macdict2.m
@@ -0,0 +1,218 @@
+#import <Foundation/Foundation.h>
+
+/**
+ * from:
+ * http://nshipster.com/dictionary-services/
+ * https://github.com/mattt/DictionaryKit
+ */
+extern DCSDictionaryRef DCSDictionaryCreate(CFURLRef url);
+extern DCSDictionaryRef DCSRecordGetSubDictionary(CFTypeRef record);
+extern CFDictionaryRef DCSCopyDefinitionMarkup(DCSDictionaryRef dictionary, CFStringRef record);
+extern CFStringRef DCSDictionaryGetName(DCSDictionaryRef dictionary);
+extern CFStringRef DCSDictionaryGetShortName(DCSDictionaryRef dictionary);
+extern CFArrayRef DCSCopyAvailableDictionaries();
+extern CFArrayRef DCSCopyRecordsForSearchString(DCSDictionaryRef dictionary, CFStringRef string, void *, void *);
+extern CFStringRef DCSRecordCopyData(CFTypeRef record);
+extern CFStringRef DCSRecordCopyDataURL(CFTypeRef record);
+extern CFStringRef DCSRecordGetAnchor(CFTypeRef record);
+extern CFStringRef DCSRecordGetAssociatedObj(CFTypeRef record);
+extern CFStringRef DCSRecordGetHeadword(CFTypeRef record);
+extern CFStringRef DCSRecordGetRawHeadword(CFTypeRef record);
+extern CFStringRef DCSRecordGetString(CFTypeRef record);
+extern CFStringRef DCSRecordGetTitle(CFTypeRef record);
+
+void NSPrint(NSString *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    NSString *string = [[NSString alloc] initWithFormat: format arguments: args];
+    va_end(args);
+    fprintf(stdout, "%s\n", [string UTF8String]);
+    [string release];
+}
+
+void NSPrintErr(NSString *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    NSString *string = [[NSString alloc] initWithFormat: format arguments: args];
+    va_end(args);
+    fprintf(stderr, "%s\n", [string UTF8String]);
+    [string release];
+}
+
+NSString *gProgramName;
+void showHelpInformation()
+{
+    NSPrint(@"Usage: %@ [-h] [-l] [[-d <dictionary>], ...] <words>", gProgramName);
+    NSPrint(@"  -h    Display this help message.");
+    NSPrint(@"  -l    Show the list of names of all available dictionaries.");
+    NSPrint(@"  -d    Specify a dictionary to search in.");
+    NSPrint(@"        Use 'all' to select all available dictionaries.");
+    NSPrint(@"        If no dictionary is specified, it will look up the word or phrase in all available and only return the first definition found.");
+}
+
+NSMapTable *availableDictionariesKeyedByName = nil;
+int setupSystemInformation()
+{
+    availableDictionariesKeyedByName = [NSMapTable
+        mapTableWithKeyOptions: NSPointerFunctionsCopyIn
+        valueOptions: NSPointerFunctionsObjectPointerPersonality];
+    for (id dictionary in (NSArray *)DCSCopyAvailableDictionaries()) {
+        NSString *name = (NSString *)DCSDictionaryGetName((DCSDictionaryRef)dictionary);
+        [availableDictionariesKeyedByName setObject: dictionary forKey: name];
+    }
+    return 0;
+}
+
+void showDictionaryList()
+{
+    @autoreleasepool {
+        for (NSString *name in availableDictionariesKeyedByName) {
+            NSPrint(@"%@", name);
+        }
+    }
+}
+
+bool gToSearchInAllDictionaries = false;
+bool gToShowDictionaryList = false;
+bool gToShowHelpInformation = false;
+int setupParameters(const int argc, const char *argv[], const NSMutableArray *words, const NSMutableSet *dicts)
+{
+    @autoreleasepool {
+        bool inOptions = false;
+        for (int i = 1; i < argc; i++) {
+            NSString *param = [NSString stringWithCString: argv[i] encoding: NSUTF8StringEncoding];
+            if (inOptions) {
+                if ([param characterAtIndex: 0] != '-') {
+                    if (!gToSearchInAllDictionaries) {
+                        if (![param isEqualToString: @"all"]) {
+                            [dicts addObject: param];
+                        } else {
+                            gToSearchInAllDictionaries = true;
+                        }
+                    }
+                    inOptions = false;
+                } else {
+                    NSPrintErr(@"ERROR:\nInvalid option value for %@: %@",
+                        [NSString stringWithCString: argv[i-1] encoding: NSUTF8StringEncoding], param);
+                    return -1;
+                }
+            } else {
+                if ([param characterAtIndex: 0] != '-') {
+                    [words addObject: param];
+                } else {
+                    if ([param isEqualToString: @"-d"]) {
+                        inOptions = true;
+                    } else if ([param isEqualToString: @"-l"]) {
+                        gToShowDictionaryList = true;
+                    } else if ([param isEqualToString: @"-h"]) {
+                        gToShowHelpInformation = true;
+                    } else {
+                        NSPrintErr(@"ERROR:\nInvalid option: %@", param);
+                        return -1;
+                    }
+                }
+            }
+        }
+        if (inOptions) {
+            NSPrintErr(@"ERROR:\nThe last option requires a value.");
+            return -1;
+        }
+        if (!gToShowDictionaryList && !gToShowHelpInformation && [words count] < 1) {
+            NSPrintErr(@"ERROR:\nPlease provide words for searching.");
+            return -1;
+        }
+    }
+    return 0;
+}
+
+int searchDictionary(const NSString *phrase, const NSMutableSet *dicts)
+{
+    @autoreleasepool {
+        if (!gToSearchInAllDictionaries && ([dicts count] == 0)) {
+            DCSDictionaryRef dictionary = NULL;
+            CFRange range = DCSGetTermRangeInString(dictionary, (CFStringRef)phrase, 0);
+            CFStringRef definition = DCSCopyTextDefinition(dictionary, (CFStringRef)phrase, range);
+            if (definition) {
+                NSPrint(@"Definitions of <%@>\n%@", phrase, (NSString *)definition);
+            } else {
+                NSPrint(@"Definitions of <%@>\n%@", phrase, @"(none)");
+            }
+        } else {
+            int totalDefinitions = 0;
+            for (id dictionaryName in (gToSearchInAllDictionaries ? availableDictionariesKeyedByName : dicts)) {
+                DCSDictionaryRef dictionary = (DCSDictionaryRef)[availableDictionariesKeyedByName objectForKey: dictionaryName];
+                CFRange range = DCSGetTermRangeInString(dictionary, (CFStringRef)phrase, 0);
+                CFStringRef definition = DCSCopyTextDefinition(dictionary, (CFStringRef)phrase, range);
+                if (range.location == kCFNotFound) {
+                    continue;
+                }
+                CFStringRef term = (CFStringRef)[phrase substringWithRange: NSMakeRange(range.location, range.length)];
+
+                if (definition) {
+                    if (totalDefinitions > 0) {
+                        NSPrint(@"\n");
+                    }
+                    NSPrint(@"Definitions of <%@> in {%@}\n%@", (NSString *)term, dictionaryName, (NSString *)definition);
+                    totalDefinitions++;
+                }
+
+                /*// alternate mode?
+                 *NSArray *records = (NSArray *)DCSCopyRecordsForSearchString(dictionary, term, NULL, NULL);
+                 *if (records) {
+                 *    for (id record in records) {
+                 *        CFStringRef headword = DCSRecordGetHeadword((CFTypeRef)record);
+                 *        if (headword) {
+                 *            CFRange range = DCSGetTermRangeInString(dictionary, headword, 0);
+                 *            CFStringRef definition = DCSCopyTextDefinition(dictionary, headword, range);
+                 *            if (definition) {
+                 *                if (totalDefinitions > 0) {
+                 *                    NSPrint(@"\n");
+                 *                }
+                 *                NSPrint(@"Definitions of <%@> in {%@}\n%@", (NSString *)headword, dictionaryName, (NSString *)definition);
+                 *                totalDefinitions++;
+                 *            }
+                 *        }
+                 *    }
+                 *}
+                 */
+            }
+            if (totalDefinitions < 1) {
+                NSPrint(@"Definitions of <%@>\n%@", phrase, @"(none)");
+            }
+        }
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    gProgramName = [NSString stringWithCString: argv[0] encoding: NSUTF8StringEncoding];
+    if (argc < 2) {
+        showHelpInformation();
+        exit(-1);
+    }
+    NSMutableArray *words = [NSMutableArray array];
+    NSMutableSet *dicts = [NSMutableSet set];
+    int exitCode = 0;
+    if ((exitCode = setupParameters(argc, (void *)argv, words, dicts))) {
+      exit(exitCode);
+    }
+    if ((exitCode = setupSystemInformation())) {
+      exit(exitCode);
+    }
+    if (gToShowHelpInformation) {
+        showHelpInformation();
+        exit(0);
+    }
+    if (gToShowDictionaryList) {
+        showDictionaryList();
+        exit(0);
+    }
+    NSString *phrase = [words componentsJoinedByString: @" "];
+    if ((exitCode = searchDictionary(phrase, dicts))) {
+        exit(exitCode);
+    }
+    exit(0);
+}
